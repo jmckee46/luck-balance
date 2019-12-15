c81c0629100674c940fb544c0cc1c59fcd7f2203
John McKee <jmckee3@mac.com>
2019-12-15 14:55:42 -0800

    working version

--- luck-balance.go
+++ luck-balance.go
@@ -1,3 +1,44 @@
 package main
 
+import "sort"
+
+type ImportantContests []int32
+
+func (a ImportantContests) Len() int           { return len(a) }
+func (a ImportantContests) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ImportantContests) Less(i, j int) bool { return a[i] < a[j] }
+
+func luckBalance(k int32, contests [][]int32) int32 {
+	var luck int32
+	var numImpContests int32
+	importantContests := make(ImportantContests, 0)
+
+	for _, value := range contests {
+		if value[1] == 0 {
+			luck += value[0]
+		} else {
+			numImpContests++
+			importantContests = append(importantContests, value[0])
+		}
+	}
+
+	// k = number of allowed important losses. k may be negative
+	if k > 0 {
+		// sort array in reverse order
+		sort.Sort(sort.Reverse(importantContests))
+		for index, value := range importantContests {
+			if int32(index+1) <= k {
+				luck += value
+			} else {
+				luck -= value
+			}
+		}
+	} else {
+		for _, value := range importantContests {
+			luck -= value
+		}
+	}
+	return luck
+}
+
 func main() {}
--- spec-luck-balance_test.go
+++ spec-luck-balance_test.go
@@ -3,6 +3,20 @@ package main
 import "testing"
 
 func TestLuckBalance(t *testing.T) {
-	// t.Errorf("got %d instead of 1", minDiff)
+	k := int32(3)
+	contests := [][]int32{
+		[]int32{5, 1},
+		[]int32{2, 1},
+		[]int32{1, 1},
+		[]int32{8, 1},
+		[]int32{10, 0},
+		[]int32{5, 0},
+	}
+
+	luck := luckBalance(k, contests)
+
+	if luck != 29 {
+		t.Errorf("got %d instead of 29", luck)
+	}
 
 }
